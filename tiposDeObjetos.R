4fa0ff7704520f7aa5e436c5d3d29b7b12ddfb13
ChristiamAlmBran <chrisvonbran@gmail.com>
2022-08-21 17:28:53 -0300

    Exercício tipos de objetos

---
+++ tiposDeObjetos.R
@@ -0,0 +1,29 @@
+install.packages('eeptools')
+library(eeptools)
+
+# vetor com nome das Organizações Internacionais
+nomeOrganizacaoInternacional <- c("UE", # União Europeia
+                                  "OEA", # Organização dos Estados Americanos
+                                  "MERCOSUL", # Mercado Comum do Sul 
+                                  "ASEAN") # Associação de Nações do Sudeste Asiático
+
+# vetor com a data de criação das Organizações Internacionais 
+criacaoOrganizacoesInternacionais <- as.Date(c("1993-11-01", 
+                                               "1948-04-30", 
+                                               "1991-03-26", 
+                                               "1967-08-08"))
+
+# vetor com as idades das Organizações Internacionais 
+idadeOrganizaçãoInternacional <- round (age_calc( criacaoOrganizacoesInternacionais,
+                                                  units = 'years'))
+
+# dataframe com base nos vetores
+
+listaOrganizacoesInternacionais <- data.frame(
+  nome = nomeOrganizacaoInternacional, # nome
+  dataCriação = criacaoOrganizacoesInternacionais, # data da criação
+  idade = idadeOrganizaçãoInternacional # idade da Organização Internacional
+)
+
+View(listaOrganizacoesInternacionais)
+
