e86c19803f8b9b78d215a7a56a21c5c9dd451c7c
ChristiamAlmBran <chrisvonbran@gmail.com>
2022-08-21 13:45:12 -0300

    Exercício Objetos e funções em R. Versão 3, comentada

--- objetosFuncoesR.R
+++ objetosFuncoesR.R
@@ -1,13 +1,20 @@
+# Ativação do pacote ggplot2
 library(ggplot2)
 
+# Objeto simples
 familyNamesAges <- data.frame (name = c("John", "Peter", "Ana"), 
                                 age = c(45, 28, 12))
+
+# Visualização do objeto simples
 View(familyNamesAges)
 
+# Criação do objeto complexo através das informações do objeto simples
 familyNamesAgesPlot <- ggplot(familyNamesAges, aes(x=name, y=age)) +
   geom_point()
 
+# Visualização do objeto complexo
 View(familyNamesAgesPlot)
 
+# Demonstração da complexidade do objeto
 str(familyNamesAgesPlot)
 
